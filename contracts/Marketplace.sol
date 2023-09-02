f54e6fd65748eb6695d195890b6fb0a3234cf4b9
Arthur Galiullin <galiullinko@gmail.com>
2023-09-02 23:23:06 +0200

    Add Registration

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -5,7 +5,9 @@
 pragma solidity ^0.8.16;
 
 import "./ERC20TransferableReceivable.sol";
+import "./TradableCashflow.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "hardhat/console.sol";
 
 // This is the main building block for smart contracts.
@@ -13,16 +15,42 @@ contract Marketplace {
     // A mapping of TradeableCashflow NFT to their ERC20TransferableReceivable
     mapping(address => address) streams;
     ERC20TransferableReceivable public receivable;
+    mapping(address => UsersTradableCashflow) public userToTradableCashflows;
+    address[] public tradableCashflowsUsers;
 
     // The Lend event helps off-chain applications understand
     // what happens within your contract.
     event Lend(address indexed _from, address indexed _to, uint256 _value);
+
+    struct UsersTradableCashflow {
+        address tradableCashflowAddress;
+        uint256 id;
+        address recievableTokenAddress;
+        uint256 discount;
+    }
 
     /**
      * Contract initialization.
      */
     constructor(ERC20TransferableReceivable _receivable) {
         receivable = _receivable;
+    }
+
+    function registerTradableCashflow(
+        IERC721 _tradableCashflow,
+        uint256 _id,
+        address _recievableTokenAddress,
+        uint256 _discount
+    ) public {
+        require(_tradableCashflow.ownerOf(_id) == msg.sender, "Not an owner");
+
+        userToTradableCashflows[msg.sender] = UsersTradableCashflow(
+            address(_tradableCashflow),
+            _id,
+            _recievableTokenAddress,
+            _discount
+        );
+        tradableCashflowsUsers.push(msg.sender);
     }
 
     /**
@@ -40,8 +68,13 @@ contract Marketplace {
     ) public {
         console.log("Creating receivable...");
 
-        // mint ERC20TransferableReceivable
+        (uint256 _startTime, address _, int96 _flowRate) = TradeableCashflow(
+            userToTradableCashflows[msg.sender].tradableCashflowAddress
+        ).currentReceiver();
+
         receivable.mint(borrower, paymentReference, amount, erc20Addr);
+
+        // mint ERC20TransferableReceivable
     }
 
     function lend(
